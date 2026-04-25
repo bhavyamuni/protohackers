8307acb884001f1625df2ab6f9bfa9dba69888c2
Bhavya Muni <bhavyamuni@outlook.com>
2026-04-25 19:24:58 -0400

    chore: add types.go

--- lineReversal/parser.go
+++ lineReversal/parser.go
@@ -6,58 +6,6 @@ import (
 	"strings"
 )
 
-type MessageType string
-
-const (
-	ConnectMessageType MessageType = "connect"
-	CloseMessageType   MessageType = "close"
-	DataMessageType    MessageType = "data"
-	AckMessageType     MessageType = "ack"
-)
-
-type Message interface {
-	String() string
-}
-
-type ConnectMessage struct {
-	MessageType
-	Session int64
-}
-
-type CloseMessage struct {
-	MessageType
-	Session int64
-}
-
-type DataMessage struct {
-	MessageType
-	Session int64
-	Pos     int64
-	Data    string
-}
-
-type AckMessage struct {
-	MessageType
-	Session int64
-	Length  int64
-}
-
-func (m *AckMessage) String() string {
-	return fmt.Sprintf("/ack/%d/%d/", m.Session, m.Length)
-}
-
-func (m *ConnectMessage) String() string {
-	return fmt.Sprintf("/connect/%d/", m.Session)
-}
-
-func (m *CloseMessage) String() string {
-	return fmt.Sprintf("/close/%d/", m.Session)
-}
-
-func (m *DataMessage) String() string {
-	return fmt.Sprintf("/data/%d/%d/%s/", m.Session, m.Pos, m.Data)
-}
-
 func ParseMessage(message string) (Message, error) {
 	messageChunks := SplitCommand(message)
 	if len(messageChunks) < 2 {
